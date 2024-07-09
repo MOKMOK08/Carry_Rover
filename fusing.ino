2890d8f690d4fdc0c3ffe4220fabfe90433aef40
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2024-07-09 17:56:27 +0900

    Add files via upload

--- fusing.ino
+++ fusing.ino
@@ -1,11 +1,15 @@
-const int FUSE_GPIO = 26;
+const int FUSE_GPIO = 2;
 
 void setup() {
   pinMode(FUSE_GPIO, OUTPUT);
-  digitalWrite(FUSE_GPIO, HIGH);
-  delay(500);
   digitalWrite(FUSE_GPIO, LOW);
 }
 
 void loop() {
+}
+
+void Fusing() {
+  digitalWrite(FUSE_GPIO, HIGH);
+  delay(500);
+  digitalWrite(FUSE_GPIO, LOW);
 }
