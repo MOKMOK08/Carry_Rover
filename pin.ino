f79a9785be8f4903b40f3c5457fafb6bf64cd3f5
Odamin3114 <sakurahykw3114@gmail.com>
2024-07-09 12:11:33 +0900

    Create pin.ino

---
+++ pin.ino
@@ -0,0 +1,11 @@
+//hayakawa
+//モータードライバピン番号未変更
+const int STBY = 2;     // モータードライバの制御の準備
+const int AIN1 = 3;     // 1つ目のDCモーターの制御
+const int AIN2 = 4;     // 1つ目のDCモーターの制御
+const int BIN1 = 7;     // 2つ目のDCモーターの制御
+const int BIN2 = 8;     // 2つ目のDCモーターの制御
+const int PWMA = 5;     // 1つ目のDCモーターの回転速度
+const int PWMB = 6;    // 2つ目のDCモーターの回転速度
+//int RX_PIN = 16;
+//int TX_PIN = 17;
