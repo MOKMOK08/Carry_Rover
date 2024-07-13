0dae38776519792b3b5bb6c4fb5155fa9b8da8f7
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2024-07-14 08:42:52 +0900

    Add files via upload

--- write_log.ino
+++ write_log.ino
@@ -1,11 +1,16 @@
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
 
 File file;
 String gps_time;
 String progress = "ready";
 String file_name = "CarryRover";
+
+HardwareSerial SerialGPS(2);
+TinyGPSPlus gps;
 
 void setup() {
   Serial.begin(115200);
@@ -39,9 +44,64 @@ void setup() {
   if (!file) {
     Serial.println("Failed to create file");
   }
+
+  SerialGPS.begin(9600, SERIAL_8N1, 16, 17);
 }
 
 void WriteLog(String data_name1 = "", String data1 = "", String data_name2 = "", String data2 = "") {
+  while (SerialGPS.available() > 0) {
+    gps.encode(SerialGPS.read());
+  }
+
+  if (gps.location.isValid() && gps.date.isValid() && gps.time.isValid()) {
+    int year = gps.date.year();
+    int month = gps.date.month();
+    int day = gps.date.day();
+    int hour = gps.time.hour();
+    int minute = gps.time.minute();
+    int second = gps.time.second();
+    int centisecond = gps.time.centisecond();
+
+    // JSTに変換
+    hour += 9;
+    if (hour >= 24) {
+      hour -= 24;
+      day += 1;
+
+      if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day > 31) {
+        day = 1;
+        month += 1;
+      } 
+      else if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+        day = 1;
+        month += 1;
+      } 
+      else if (month == 2) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+          if (day > 29) {
+            day = 1;
+            month += 1;
+          }
+        } 
+        else {
+          if (day > 28) {
+            day = 1;
+            month += 1;
+          }
+        }
+      }
+
+      if (month > 12) {
+        month = 1;
+        year += 1;
+      }
+    }
+
+  char buffer[30];
+  sprintf(buffer, "%04d-%02d-%02d %02d:%02d:%02d.%02d", year, month, day, hour, minute, second, centisecond);
+  gps_time = String(buffer);
+  }
+
   file = SD.open(file_name.c_str(), FILE_APPEND);
   file.print(gps_time);
   file.print(',');
