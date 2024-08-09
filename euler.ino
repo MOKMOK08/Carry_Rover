675fcb87c70fe6ea7e194707f90347cd61100cfc
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2024-08-10 00:11:10 +0900

    Add files via upload

--- euler.ino
+++ euler.ino
@@ -1,8 +1,6 @@
-#include <Wire.h>
 #include <Adafruit_BNO055.h>
 
 // BNO055の設定
-double eulerdata[3];
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 void setup() {
@@ -21,48 +19,45 @@ void setup() {
 }
 
 void loop() {
-  Euler();
   Serial.print("φ = ");
-  Serial.println(eulerdata[0]);
+  Serial.println(Euler(0));
   Serial.print("θ = ");
-  Serial.println(eulerdata[1]);
+  Serial.println(Euler(1));
   Serial.print("𝜓 = ");
-  Serial.println(eulerdata[2]);
+  Serial.println(Euler(2));
   delay(100);
 }
 
 // クオータニオンをオイラー角に変換
-void Euler(){
+double Euler(int axis) {
+  double roll = 0;
+  double pitch = 0;
+  double yaw = 0;
+
   imu::Quaternion quat = bno.getQuat();
   double w = quat.w();
   double x = quat.x();
   double y = quat.y();
   double z = quat.z();
-
   double ysqr = y * y;
 
-  // roll (x-axis rotation)
-  double t0 = +2.0 * (w * x + y * z);
-  double t1 = +1.0 - 2.0 * (x * x + ysqr);
-  double roll = atan2(t0, t1);
-
-  // pitch (y-axis rotation)
-  double t2 = +2.0 * (w * y - z * x);
-  t2 = t2 > 1.0 ? 1.0 : t2;
-  t2 = t2 < -1.0 ? -1.0 : t2;
-  double pitch = asin(t2);
-
-  // yaw (z-axis rotation)
-  double t3 = +2.0 * (w * z + x * y);
-  double t4 = +1.0 - 2.0 * (ysqr + z * z);  
-  double yaw = atan2(t3, t4);
-
-  //ラジアンから度に変換
-  roll *= 57.2957795131;
-  pitch *= 57.2957795131;
-  yaw *= 57.2957795131;
-
-  eulerdata[0] = roll;
-  eulerdata[1] = pitch;   
-  eulerdata[2] = yaw;
+  if(axis == 0) {
+    double t0 = +2.0 * (w * x + y * z);
+    double t1 = +1.0 - 2.0 * (x * x + ysqr);
+    roll = atan2(t0, t1) * 57.2957795131;
+    return roll;
+  }
+  else if(axis == 1){
+    double t2 = +2.0 * (w * y - z * x);
+    t2 = t2 > 1.0 ? 1.0 : t2;
+    t2 = t2 < -1.0 ? -1.0 : t2;
+    pitch = asin(t2) * 57.2957795131;
+    return pitch;
+  }
+  else if(axis == 2){
+    double t3 = +2.0 * (w * z + x * y);
+    double t4 = +1.0 - 2.0 * (ysqr + z * z);  
+    yaw = atan2(t3, t4) * 57.2957795131;
+    return yaw;
+  }
 }
